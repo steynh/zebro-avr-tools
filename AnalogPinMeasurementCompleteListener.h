4d76744920a387fdd25a2f95981b6e508d1e32ae
Steyn Huurman <github@steynhuurman.nl>
2017-07-23 00:51:26 +0900

    Added contact information

--- AnalogPinMeasurementCompleteListener.h
+++ AnalogPinMeasurementCompleteListener.h
@@ -1,6 +1,8 @@
-//
-// Created by steyn on 17-6-4.
-//
+/*
+ * Created by Steyn Huurman on 04-06-2017
+ *
+ * You can ask me questions: zebro@steynhuurman.nl
+ */
 
 #ifndef ZEBRO_AVR_ANALOG_PIN_MEASUREMENT_COMPLETE_LISTENER_H
 #define ZEBRO_AVR_ANALOG_PIN_MEASUREMENT_COMPLETE_LISTENER_H
